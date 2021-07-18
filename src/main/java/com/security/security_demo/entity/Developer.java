05763c04de13239736df44e80818ca92e7adb3d3
mamontinnokent <61653414+mamontinnokent@users.noreply.github.com>
2021-07-18 14:41:23 +0300

    Создание шаблона

---
+++ src/main/java/com/security/security_demo/entity/Developer.java
@@ -0,0 +1,12 @@
+package com.security.security_demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
---
+++ src/main/java/com/security/security_demo/rest/DeveloperRestController.java
@@ -0,0 +1,34 @@
+package com.security.security_demo.rest;
+
+import com.security.security_demo.entity.Developer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestController {
+    private List<Developer> DEVELOPERS = Stream.of(
+            new Developer(1l, "Ivan", "Ivanov"),
+            new Developer(2l, "Petr", "Petrov"),
+            new Developer(3l, "Sergey", "Sergeev")
+    ).collect(Collectors.toList());
+
+
+@GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
